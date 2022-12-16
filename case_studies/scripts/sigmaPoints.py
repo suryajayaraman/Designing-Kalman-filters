014d10831df4bbb5f90778a575e8e3aacdbca66e
suryajayaraman <suryajayaraman97@gmail.com>
2022-12-16 06:10:36 +0530

    Added simple UT for sigma point generator

--- case_studies/scripts/sigmaPoints.py
+++ case_studies/scripts/sigmaPoints.py
@@ -1,6 +1,7 @@
 
+import scipy
 import numpy as np
-
+from scipy.linalg import cholesky
 
 class MerweSigmaPointGenerator:
 
@@ -19,7 +20,7 @@ class MerweSigmaPointGenerator:
 
         # intermediate variables
         self.numSigmaPoints = 2*n + 1
-        self._lambda  =  self.alpha**2 (self.n + self.kappa) - n
+        self._lambda  =  (self.alpha**2) * (self.n + self.kappa) - self.n
 
         # weights for means
         self.Wm = np.zeros(self.n)
@@ -39,4 +40,20 @@ class MerweSigmaPointGenerator:
 
         # initialize return variable
         sigmaPoints = np.zeros((self.numSigmaPoints, self.n))
+
+        # first sigma point is mean
+        sigmaPoints[0] = x[:]
+        
+        # Matrix root of variance using choleshky decomposition
+        P_sqrt = cholesky((self.n + self._lambda) * P)
+        
+        # other sigma points
+        sigmaPoints[1:self.n+1, :] = x + P_sqrt
+        sigmaPoints[self.n+1:, :]  = x - P_sqrt            
         return sigmaPoints
+
+
+if __name__ == "__main__":
+    np.set_printoptions(suppress=True)
+    points = MerweSigmaPointGenerator(n=1, alpha=0.1, beta=2.0, kappa=1)
+    print(points.sigma_points(x=np.array([0,0]), P = np.array([[1,0.1],[0.1,1]])))
